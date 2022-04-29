8bf934b14ecc463609ea1b1928ca76806a4d2b15
Peterhague <prhague@hotmail.co.uk>
2022-04-29 07:32:23 +0000

    added / to views.py

--- stack/views.py
+++ stack/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {'name' : 'Peter'}
+    return render(request, '/index.html', context)
 
